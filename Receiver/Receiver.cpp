2b6e32b06475d935a0db9eb0b88f0d11ffa4fea9
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-09-13 09:58:46 +0530

    Update Receiver.cpp

--- Receiver/Receiver.cpp
+++ Receiver/Receiver.cpp
@@ -17,7 +17,7 @@ Receiver::Receiver(int dy, int dt, int m, int y, float wa) : day(dy), date(dt), 
 
 Receiver::Receiver(int i, int hr, int min, int sec, int dy, int dt, int mon, int yr) : id(i), hour(hr), minute(min), second(sec), day(dy), date(dt), month(mon), year(yr) {}
 
-void Receiver::readSenderData()
+/*void Receiver::readSenderData()
 {
 	string footfallRecordString;
 	getline(cin, footfallRecordString);
@@ -27,9 +27,23 @@ void Receiver::readSenderData()
 		cout << footfallRecordString << endl;
 		storeFootfallData(footfallRecordString);
 	}
+}*/
+
+vector<Receiver> Receiver::readSenderData()
+{
+	vector<Receiver> footfallData;
+	string footfallRecordString;
+	getline(cin, footfallRecordString);
+	cout << footfallRecordString << endl;
+	while (getline(cin, footfallRecordString))
+	{
+		cout << footfallRecordString << endl;
+		footfallData = storeFootfallData(footfallRecordString, footfallData);
+	}
+	return footfallData;
 }
 
-void Receiver::storeFootfallData(const string& footfallRecordString)
+/*void Receiver::storeFootfallData(const string& footfallRecordString)
 {
 	vector<int> footfallRecord;
 	string footfallElement;
@@ -43,17 +57,34 @@ void Receiver::storeFootfallData(const string& footfallRecordString)
 	cout << footfallRecord.size() << endl;
 	Receiver receiverObj(footfallRecord[0], footfallRecord[1], footfallRecord[2], footfallRecord[3], footfallRecord[4], footfallRecord[5], footfallRecord[6], footfallRecord[7]);
 	footfallData.push_back(receiverObj);
+}*/
+
+vector<Receiver> Receiver::storeFootfallData(const string& footfallRecordString, vector<Receiver> footfallData)
+{
+	vector<int> footfallRecord;
+	string footfallElement;
+	stringstream footfallStream(footfallRecordString);
+	while (getline(footfallStream, footfallElement, ','))
+	{
+		footfallRecord.push_back(stoi(footfallElement));
+	}
+	
+	cout << "Record Pushed" << endl;
+	cout << footfallRecord.size() << endl;
+	Receiver receiverObj(footfallRecord[0], footfallRecord[1], footfallRecord[2], footfallRecord[3], footfallRecord[4], footfallRecord[5], footfallRecord[6], footfallRecord[7]);
+	footfallData.push_back(receiverObj);
+	return footfallData;
 }
 
-void Receiver::averageFootfallsPerHourDaily()
+/*void Receiver::averageFootfallsPerHourDaily()
 {
-	/*ofstream fout("test-data/Average Hourly Footfalls Daily.csv", ios::app);
-	if(fout.is_open())
-	{
-		cout << "File created" << endl;
-		fout << "11" << ", " << "123" << ", " << "45" <<"\n";
-	}
-	fout.close();*/
+	//ofstream fout("test-data/Average Hourly Footfalls Daily.csv", ios::app);
+	//if(fout.is_open())
+	//{
+	//	cout << "File created" << endl;
+	//	fout << "11" << ", " << "123" << ", " << "45" <<"\n";
+	//}
+	//fout.close();
 	vector<Receiver> hourlyAverageDailyData;
 	Receiver receiverObj(footfallData[0].date, footfallData[0].month, footfallData[0].year, 0);
 	for(unsigned int i = 0; i < footfallData.size(); i++)
@@ -77,6 +108,33 @@ void Receiver::averageFootfallsPerHourDaily()
 	hourlyAverageDailyData.push_back(receiverObj);
 	
 	displayHourlyAverageDailyData(hourlyAverageDailyData);
+}*/
+
+vector<Receiver> Receiver::averageFootfallsPerHourDaily(vector<Receiver> footfallData)
+{
+	vector<Receiver> hourlyAverageDailyData;
+	Receiver receiverObj(footfallData[0].date, footfallData[0].month, footfallData[0].year, 0);
+	for(unsigned int i = 0; i < footfallData.size(); i++)
+	{
+		if(receiverObj.date == footfallData[i].date)
+		{
+			receiverObj.hourlyAverage += 1;
+		}
+		else
+		{
+			receiverObj.hourlyAverage = receiverObj.hourlyAverage / 4;
+			hourlyAverageDailyData.push_back(receiverObj);
+			receiverObj.date = footfallData[i].date;
+			receiverObj.month = footfallData[i].month;
+			receiverObj.year = footfallData[i].year;
+			receiverObj.hourlyAverage = 0;
+			i--;
+		}
+	}
+	receiverObj.hourlyAverage = receiverObj.hourlyAverage / 4;
+	hourlyAverageDailyData.push_back(receiverObj);
+	
+	return hourlyAverageDailyData;
 }
 
 int Receiver::setFlagStatus(int day)
@@ -86,7 +144,7 @@ int Receiver::setFlagStatus(int day)
 	return 1;
 }
 
-void Receiver::averageDailyFootfallsWeekly()
+/*void Receiver::averageDailyFootfallsWeekly()
 {
 	vector<Receiver> dailyAverageWeeklyData;
 	Receiver receiverObj(footfallData[0].day, footfallData[0].date, footfallData[0].month, footfallData[0].year, 0);
@@ -116,28 +174,91 @@ void Receiver::averageDailyFootfallsWeekly()
 	dailyAverageWeeklyData.push_back(receiverObj);
 	
 	displayDailyAverageWeeklyData(dailyAverageWeeklyData);
+}*/
+
+vector<Receiver> Receiver::averageDailyFootfallsWeekly(vector<Receiver> footfallData)
+{
+	vector<Receiver> dailyAverageWeeklyData;
+	Receiver receiverObj(footfallData[0].day, footfallData[0].date, footfallData[0].month, footfallData[0].year, 0);
+	int flag = 0;
+
+	for(unsigned int i = 0; i < footfallData.size(); i++)
+	{
+		if(footfallData[i].day != flag)
+		{
+			receiverObj.weeklyAverage += 1;
+			flag = setFlagStatus(footfallData[i].day);
+		}
+		else
+		{
+			receiverObj.weeklyAverage = receiverObj.weeklyAverage / 7;
+			dailyAverageWeeklyData.push_back(receiverObj);
+			receiverObj.day = footfallData[i].day;
+			receiverObj.date = footfallData[i].date;
+			receiverObj.month = footfallData[i].month;
+			receiverObj.year = footfallData[i].year;
+			receiverObj.weeklyAverage = 0;
+			flag = 0;
+			i--;	
+		}
+	}
+	receiverObj.weeklyAverage = receiverObj.weeklyAverage / 7;
+	dailyAverageWeeklyData.push_back(receiverObj);
+	
+	return dailyAverageWeeklyData;
 }
 
-
-void Receiver::displayHourlyAverageDailyData(vector<Receiver> hourlyAverageDailyData)
+/*void Receiver::displayHourlyAverageDailyData(vector<Receiver> hourlyAverageDailyData)
 {
 	cout<<"Date "<<"Month "<<"Year "<<" Hourly Avg"<<endl;
 	for(unsigned int i = 0; i < hourlyAverageDailyData.size(); i++)
 	{
 		cout<<hourlyAverageDailyData[i].date<<" "<<hourlyAverageDailyData[i].month<<" "<<hourlyAverageDailyData[i].year<<" "<<hourlyAverageDailyData[i].hourlyAverage<<endl;
 	}
+}*/
+
+void Receiver::displayHourlyAverageDailyData(vector<Receiver> hourlyAverageDailyData)
+{
+	ofstream fout("test-data/Average Footfalls Hourly.csv");
+	cout<<"Date "<<"Month "<<"Year "<<" Hourly Avg"<<endl;
+	if(fout.is_open())
+	{
+		fout << "Date" << "," << "Month" << "," << "Year" << "," << "HourlyAverage" <<"\n";
+		for(unsigned int i = 0; i < hourlyAverageDailyData.size(); i++)
+		{
+			cout<<hourlyAverageDailyData[i].date<<" "<<hourlyAverageDailyData[i].month<<" "<<hourlyAverageDailyData[i].year<<" "<<hourlyAverageDailyData[i].hourlyAverage<<endl;
+			fout << hourlyAverageDailyData[i].date << "," << hourlyAverageDailyData[i].month << "," << hourlyAverageDailyData[i].year << "," << hourlyAverageDailyData[i].hourlyAverage <<"\n";
+		}
+	}
+	fout.close();
 }
 
-void Receiver::displayDailyAverageWeeklyData(vector<Receiver> dailyAverageWeeklyData)
+/*void Receiver::displayDailyAverageWeeklyData(vector<Receiver> dailyAverageWeeklyData)
 {
 	cout<<"Date "<<"Month "<<"Year "<<" Weekly Avg"<<endl;
 	for(unsigned int i = 0; i < dailyAverageWeeklyData.size(); i++)
 	{
 		cout<<dailyAverageWeeklyData[i].date<<" "<<dailyAverageWeeklyData[i].month<<" "<<dailyAverageWeeklyData[i].year<<" "<<dailyAverageWeeklyData[i].weeklyAverage<<endl;
 	}
+}*/
+
+void Receiver::displayDailyAverageWeeklyData(vector<Receiver> dailyAverageWeeklyData)
+{
+	ofstream fout("test-data/Average Footfalls Weekly.csv");
+	cout<<"Date "<<"Month "<<"Year "<<" Weekly Avg"<<endl;
+	if(fout.is_open())
+	{
+		fout << "Date" << "," << "Month" << "," << "Year" << "," << "WeeklyAverage" <<"\n";
+		for(unsigned int i = 0; i < dailyAverageWeeklyData.size(); i++)
+		{
+			cout<<dailyAverageWeeklyData[i].date<<" "<<dailyAverageWeeklyData[i].month<<" "<<dailyAverageWeeklyData[i].year<<" "<<dailyAverageWeeklyData[i].weeklyAverage<<endl;
+			fout<<dailyAverageWeeklyData[i].date<<","<<dailyAverageWeeklyData[i].month<<","<<dailyAverageWeeklyData[i].year<<","<<dailyAverageWeeklyData[i].weeklyAverage<<"\n";
+		}
+	}
+	fout.close();
 }
 
-void Receiver::peakDailyFootfallLastMonth()
+/*void Receiver::peakDailyFootfallLastMonth()
 {
 	vector<Receiver> dailyFootfallLastMonthData;
 	vector<Receiver> footfallLastMonthData = getLastMonthFootfallData();
@@ -163,9 +284,53 @@ void Receiver::peakDailyFootfallLastMonth()
 	vector<Receiver> peakDailyFootfallsLastMonthData = getPeakDailyFootfallsLastMonth(dailyFootfallLastMonthData);
 	
 	displayPeakDailyFootfallLastMonth(peakDailyFootfallsLastMonthData);
+}*/
+
+vector<Receiver> Receiver::peakDailyFootfallLastMonth(vector<Receiver> footfallData)
+{
+	vector<Receiver> dailyFootfallLastMonthData;
+	vector<Receiver> footfallLastMonthData = getLastMonthFootfallData(footfallData);
+	Receiver receiverObj(footfallLastMonthData[0].date, footfallLastMonthData[0].month, footfallLastMonthData[0].year, 0);
+	for(unsigned int i = 0; i < footfallLastMonthData.size(); i++)
+	{
+		if(receiverObj.date == footfallLastMonthData[i].date)
+		{
+			receiverObj.dailyCount += 1;
+		}
+		else
+		{
+			dailyFootfallLastMonthData.push_back(receiverObj);
+			receiverObj.date = footfallData[i].date;
+			receiverObj.month = footfallData[i].month;
+			receiverObj.year = footfallData[i].year;
+			receiverObj.dailyCount = 0;
+			i--;
+		}
+	}
+	dailyFootfallLastMonthData.push_back(receiverObj);
+	
+	vector<Receiver> peakDailyFootfallsLastMonthData = getPeakDailyFootfallsLastMonth(dailyFootfallLastMonthData);
+	
+	return peakDailyFootfallsLastMonthData;
 }
 
-vector<Receiver> Receiver::getLastMonthFootfallData()
+/*vector<Receiver> Receiver::getLastMonthFootfallData()
+{
+	time_t currentTime = time(0);
+	tm *localTime = localtime(&currentTime);
+	int lastMonth = localTime->tm_mon;
+	vector<Receiver> lastMonthFootfallData;
+	for(unsigned int i = 0; i < footfallData.size(); i++)
+	{
+		if(lastMonth == footfallData[i].month)
+		{
+			lastMonthFootfallData.push_back(footfallData[i]);
+		}
+	}
+	return lastMonthFootfallData;
+}*/
+
+vector<Receiver> Receiver::getLastMonthFootfallData(vector<Receiver> footfallData)
 {
 	time_t currentTime = time(0);
 	tm *localTime = localtime(&currentTime);
@@ -199,13 +364,29 @@ vector<Receiver> Receiver::getPeakDailyFootfallsLastMonth(vector<Receiver> daily
 	return peakDailyFootfallsLastMonth;
 }
 
-void Receiver::displayPeakDailyFootfallLastMonth(vector<Receiver> peakDailyFootfallsLastMonthData)
+/*void Receiver::displayPeakDailyFootfallLastMonth(vector<Receiver> peakDailyFootfallsLastMonthData)
 {
 	cout<<"Date "<<"Month "<<"Year "<<"Peak Count"<<endl;
 	for(unsigned int i = 0; i < peakDailyFootfallsLastMonthData.size(); i++)
 	{
 		cout<<peakDailyFootfallsLastMonthData[i].date<<" "<<peakDailyFootfallsLastMonthData[i].month<<" "<<peakDailyFootfallsLastMonthData[i].year<<" "<<peakDailyFootfallsLastMonthData[i].dailyCount<<endl;
 	}
+}*/
+
+void Receiver::displayPeakDailyFootfallLastMonth(vector<Receiver> peakDailyFootfallsLastMonthData)
+{
+	ofstream fout("test-data/Peak Daily Footfalls Last Month.csv");
+	cout<<"Date "<<"Month "<<"Year "<<"Peak Count"<<endl;
+	if(fout.is_open())
+	{
+		fout << "Date" << "," << "Month" << "," << "Year" << "," << "PeakCount" <<"\n";
+		for(unsigned int i = 0; i < peakDailyFootfallsLastMonthData.size(); i++)
+		{
+			cout<<peakDailyFootfallsLastMonthData[i].date<<" "<<peakDailyFootfallsLastMonthData[i].month<<" "<<peakDailyFootfallsLastMonthData[i].year<<" "<<peakDailyFootfallsLastMonthData[i].dailyCount<<endl;
+			fout<<peakDailyFootfallsLastMonthData[i].date<<","<<peakDailyFootfallsLastMonthData[i].month<<","<<peakDailyFootfallsLastMonthData[i].year<<","<<peakDailyFootfallsLastMonthData[i].dailyCount<<"\n";
+		}
+	}
+	fout.close();
 }
 
 int main()
@@ -214,9 +395,16 @@ int main()
 	
 	cout << "In receiver main" << endl;
 	
-	receiverObj.readSenderData();
-	receiverObj.averageFootfallsPerHourDaily();
-	receiverObj.averageDailyFootfallsWeekly();
-	receiverObj.peakDailyFootfallLastMonth();
+	vector<Receiver> footfallData = receiverObj.readSenderData();
+	
+	vector<Receiver> averageFootfallsPerHourDailyResult = receiverObj.averageFootfallsPerHourDaily(footfallData);
+	receiverObj.displayHourlyAverageDailyData(averageFootfallsPerHourDailyResult);
+	
+	vector<Receiver> averageDailyFootfallsWeeklyResult = receiverObj.averageDailyFootfallsWeekly(footfallData);
+	receiverObj.displayDailyAverageWeeklyData(averageDailyFootfallsWeeklyResult);
+	
+	vector<Receiver> peakDailyFootfallLastMonthResult = receiverObj.peakDailyFootfallLastMonth(footfallData);
+	receiverObj.displayPeakDailyFootfallLastMonth(peakDailyFootfallLastMonthResul);
+	
 	return 0;
 }
