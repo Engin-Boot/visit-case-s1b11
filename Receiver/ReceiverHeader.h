8cb8b0f79903b7d0e1a1e6fbfac9814d0aeb9a20
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-09-13 10:04:11 +0530

    Update ReceiverHeader.h

--- Receiver/ReceiverHeader.h
+++ Receiver/ReceiverHeader.h
@@ -20,22 +20,6 @@ private:
 	float hourlyAverage = 0;
 	float weeklyAverage = 0;
 public:
-	/*Receiver();
-	Receiver(int, int, int, float);
-	Receiver(int, int, int, int, float);
-	Receiver(int, int, int, int, int, int, int, int);
-	void readSenderData();
-	void storeFootfallData(const string&);
-	void averageFootfallsPerHourDaily();
-	void displayHourlyAverageDailyData(vector<Receiver>);
-	void displayDailyAverageWeeklyData(vector<Receiver>);
-	void averageDailyFootfallsWeekly();
-	int setFlagStatus(int);
-	void peakDailyFootfallLastMonth();
-	vector<Receiver> getLastMonthFootfallData();
-	vector<Receiver> getPeakDailyFootfallsLastMonth(vector<Receiver>);
-	void displayPeakDailyFootfallLastMonth(vector<Receiver>);*/
-	
 	Receiver();
 	Receiver(int, int, int, float);
 	Receiver(int, int, int, int, float);
