d42474161939755204969e4c0950cbd87ac79ef6
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-15 13:00:05 +0530

    Update SenderTest.cpp

--- Sender-Test/SenderTest.cpp
+++ Sender-Test/SenderTest.cpp
@@ -7,7 +7,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+//visitdataout1.txt is the file to which sender writes ffotfall data after printing the same to the console
 TEST_CASE("when footfall data containing all valid data is read by sender then it prints all the valid data") 
 {
      Sender::fetchValidateandPrintFootfallData("test-data/visitdata1.csv");
@@ -38,5 +38,17 @@ TEST_CASE("when footfall data containing large number of more invalid data is re
     std::ifstream fin2("OutputSenderTestData/visitdataout1.txt");
     std::string obtainedData((std::istreambuf_iterator<char>(fin2)), std::istreambuf_iterator<char>());
     fin2.close();
-    REQUIRE(obtainedData == "\n");    
+     REQUIRE(obtainedData == "No valid data");    
+}
+
+TEST_CASE("when a non-existing footfall data csv file is read by sender then it prints file doesn't exist to console") 
+{
+     Sender::fetchValidateandPrintFootfallData("test-data/xyz.csv");
+     /*std::ifstream fin1("OutputSenderTestData/nodata.txt");
+    std::string testData((std::istreambuf_iterator<char>(fin1)), std::istreambuf_iterator<char>());
+    fin1.close();*/
+    std::ifstream fin2("OutputSenderTestData/visitdataout1.txt");
+    std::string obtainedData((std::istreambuf_iterator<char>(fin2)), std::istreambuf_iterator<char>());
+    fin2.close();
+    REQUIRE(obtainedData == "file doesn't exist");    
 }
