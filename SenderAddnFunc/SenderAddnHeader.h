286a59af9099c1cd10ee9377c2c7f3a2274ef3a4
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-15 18:15:57 +0530

    Create SenderAddnHeader.h

---
+++ SenderAddnFunc/SenderAddnHeader.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <string>
+#include<sstream>
+#include <algorithm>
+
+class CSVReader
+{
+    std::string fileName;
+    std::string delimeter;
+public:
+    CSVReader(const std::string& filename, const std::string& delim = ",") : fileName(filename), delimeter(delim) { }
+    // Function to fetch data from a CSV File
+    std::vector<std::vector<std::string>> fetchActualFootfallData();
+};
+
+namespace Sender
+{
+    void fetchValidateReconcileandPrintFootfallData(std::string,std::string);
+}
