7878ce3ea61d7c034a4a7edc369a71bb2c4c39cb
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-15 19:49:13 +0530

    Update SenderExtTest.cpp

--- SenderExtFun-Test/SenderExtTest.cpp
+++ SenderExtFun-Test/SenderExtTest.cpp
@@ -10,7 +10,10 @@
 
 TEST_CASE("when sender reads both sensor and manual data then it reconciles the sensor data, inserts the missed entries, writes to the txt file and prints the same to the console") 
 {
-     Sender::fetchValidateReconcileandPrintFootfallData("test-data/visitdata2.csv","test-data/manuallog2.csv");
+     std::string filename = "test-data/visitdata2.csv";
+	std::string manuallog = "test-data/manuallog2.csv";
+	Sender::fetchValidateReconcileandPrintFootfallData(filename,manuallog);
+     //Sender::fetchValidateReconcileandPrintFootfallData("test-data/visitdata2.csv","test-data/manuallog2.csv");
      
     std::ifstream fin1("OutputSenderTestData/reconciletest2.txt"); //testing file
     std::string testData((std::istreambuf_iterator<char>(fin1)), std::istreambuf_iterator<char>());
@@ -23,7 +26,10 @@ TEST_CASE("when sender reads both sensor and manual data then it reconciles the 
 
 TEST_CASE("when sender reads both sensor and manual data and sensor data has more invalid entries then it takes only manual data assuming that sensor is malfunctioning, writes to the txt file and prints the same to the console") 
 {
-     Sender::fetchValidateReconcileandPrintFootfallData("test-data/invalidvisitdata1.csv","test-data/manuallog2.csv");
+     std::string filename = "test-data/invalidvisitdata1.csv";
+     std::string manuallog = "test-data/manuallog2.csv";
+     Sender::fetchValidateReconcileandPrintFootfallData(filename,manuallog);
+     //Sender::fetchValidateReconcileandPrintFootfallData("test-data/invalidvisitdata1.csv","test-data/manuallog2.csv");
      
     std::ifstream fin1("OutputSenderTestData/reconciletest2.txt"); //testing file
     std::string testData((std::istreambuf_iterator<char>(fin1)), std::istreambuf_iterator<char>());
@@ -36,7 +42,10 @@ TEST_CASE("when sender reads both sensor and manual data and sensor data has mor
 
 TEST_CASE("when sender reads both sensor and manual data and either of them are not existing files then it file doesn't exist to the console") 
 {
-     Sender::fetchValidateReconcileandPrintFootfallData("abc.csv","test-data/xyz.csv");
+    std::string filename = "abc.csv";
+     std::string manuallog = "test-data/xyz.csv";
+     Sender::fetchValidateReconcileandPrintFootfallData(filename,manuallog);
+     //Sender::fetchValidateReconcileandPrintFootfallData("abc.csv","test-data/xyz.csv");
     std::ifstream fin2("OutputSenderTestData/visitdataout1.txt"); // sender writes to  this
     std::string obtainedData((std::istreambuf_iterator<char>(fin2)), std::istreambuf_iterator<char>());
     fin2.close();
