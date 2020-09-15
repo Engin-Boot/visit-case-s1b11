8c4ad625e682d242be889ee81624be78631c6e64
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-09-15 18:44:15 +0530

    Update ReceiverTest.cpp

--- Receiver-Test/ReceiverTest.cpp
+++ Receiver-Test/ReceiverTest.cpp
@@ -112,7 +112,7 @@ TEST_CASE("(Data 1) Peak daily footfalls last month") {
     ifstream fin1("test-data/Peak Daily Footfalls Last Month Test.csv");
     string testDataBuffer((istreambuf_iterator<char>(fin1)), istreambuf_iterator<char>());
     fin1.close();
-    ifstream fin2("test-data/test-data/Peak Daily Footfalls Last Month.csv");
+    ifstream fin2("test-data/Peak Daily Footfalls Last Month.csv");
     string actualDataBuffer((istreambuf_iterator<char>(fin2)), istreambuf_iterator<char>());
     fin2.close();
     REQUIRE(testDataBuffer == actualDataBuffer);
