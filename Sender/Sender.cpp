40b5d377d653fd720e4f4a044e3cd1bfc6383ad9
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-16 07:50:28 +0530

    Update Sender.cpp

--- Sender/Sender.cpp
+++ Sender/Sender.cpp
@@ -113,6 +113,8 @@ void Sender::fetchValidateandPrintFootfallData(std::string filename)
 
 int main()
 {
+	//"test-data/visitdata2.csv" contains all the data in "test-data/visitdata1.csv" and all the newly added entries are invalid
+	// so sender prints the data same as in visitdata1.csv after validating
     Sender::fetchValidateandPrintFootfallData("test-data/visitdata2.csv"); 
     return 0;
 }
