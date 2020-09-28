4001376ce5d29f02869aaef5a8b5687ffabc4d12
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-09-28 17:19:37 +0530

    Update Receiver.cpp

--- Receiver/Receiver.cpp
+++ Receiver/Receiver.cpp
@@ -231,29 +231,4 @@ void Receiver::displayPeakDailyFootfallLastMonth(vector<Receiver> peakDailyFootf
 	fout.close();
 }
 
-int main()
-{
-	Receiver receiverObj;
-	
-	cout << "In receiver main" << endl;
-	
-	vector<Receiver> footfallData = receiverObj.readSenderData();
-	
-	if(footfallData.size() != 0) // if sender data is available
-	{
-		vector<Receiver> averageFootfallsPerHourDailyResult = receiverObj.averageFootfallsPerHourDaily(footfallData);
-		receiverObj.displayHourlyAverageDailyData(averageFootfallsPerHourDailyResult);
-	
-		vector<Receiver> averageDailyFootfallsWeeklyResult = receiverObj.averageDailyFootfallsWeekly(footfallData);
-		receiverObj.displayDailyAverageWeeklyData(averageDailyFootfallsWeeklyResult);
-	
-		vector<Receiver> peakDailyFootfallLastMonthResult = receiverObj.peakDailyFootfallLastMonth(footfallData);
-		receiverObj.displayPeakDailyFootfallLastMonth(peakDailyFootfallLastMonthResult);
-	}
-	else
-	{
-		cout<<"No data."<<endl;
-	}
-	
-	return 0;
-}
+
